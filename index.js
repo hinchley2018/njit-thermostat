29132cc5b0d70bae21348eeee3dffe2a1f8f6e68
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2022-05-02 16:46:43 -0500

    add starter code

---
+++ index.js
@@ -0,0 +1,7 @@
+// Note: We'll be using the Fahrenheit scale for this assignment!
+
+// This code sets a random starting current temperature between 0 and 100 degrees
+let currentTemp = Math.round(Math.random() * 100)
+
+// Give the user of the thermostat a status message
+console.log(`The current temperature is ${currentTemp}°F`)
